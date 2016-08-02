7fa50da383c74c2c69a90842ccf7d9fb46074f0d
Monserrat Mora <monserratmorab@gmail.com>
2016-08-01 22:07:12 -0400

    se eliminan archivos no utilizados

--- jMetal Practica2/jmetal/util/comparators/IConstraintViolationComparator.java
+++
@@ -1,9 +0,0 @@
-package jmetal.util.comparators;
-
-import jmetal.core.Solution;
-import java.util.Comparator;
-
-public interface IConstraintViolationComparator extends Comparator {
-    public int compare(Object o1, Object o2);
-    public boolean needToCompare(Solution s1, Solution s2);
-}
